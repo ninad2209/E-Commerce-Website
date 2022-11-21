c51126ed7f33bbd9776a19382695b8f8c1c8d73d
Pratik Patil <pratikpatil9527@gmail.com>
2022-11-21 18:58:13 +0530

    first commit

---
+++ ecommerce/ecommapp/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from .models import Product,Contact
+from math import ceil
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+   # products = Product.objects.all()
+   # print(products)
+   # n = len(products)
+   # nSlides = n//4 + ceil((n/4)-(n//4))
+
+   allProds = []
+   catprods = Product.objects.values('category', 'id')
+   cats = {item['category'] for item in catprods}
+   for cat in cats:
+      prod = Product.objects.filter(category=cat)
+      n = len(prod)
+      nSlides = n // 4 + ceil((n / 4) - (n // 4))
+      allProds.append([prod, range(1, nSlides), nSlides])
+
+   # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+   # allProds = [[products, range(1, nSlides), nSlides],
+   #             [products, range(1, nSlides), nSlides]]
+   params = {'allProds':allProds}
+   return render(request, 'ecommapp/index.html', params)
+
+def about(request):
+   return render(request, 'ecommapp/about.html')
+
+def contact(request):
+      if request.method=="POST":
+         print(request)
+      name=request.POST.get('name', '')
+      email=request.POST.get('email', '')
+      phone=request.POST.get('phone', '')
+      desc=request.POST.get('desc', '')
+      contact = Contact(name=name, email=email, phone=phone, desc=desc)
+      contact.save()
+      return render(request, 'ecommapp/contact.html')
+
+def tracker(request):
+   return render(request, 'ecommapp/tracker.html')
+
+def search(request):
+   return render(request, 'ecommapp/search.html')
+
+def productview(request, myid):
+   # Fetch the product using the id
+   product = Product.objects.filter(id=myid)
+
+
+   return render(request, 'ecommapp/prodView.html', {'product':product[0]})
+
+def checkout(request):
+   return render(request, 'ecommapp/checkout.html')
